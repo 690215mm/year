b4f4498a5cb38033a5565a57b44337d89d86b83c
JASON <meiline00@gmail.com>
2022-05-26 16:45:31 +0800

    basic version

--- year.py
+++ year.py
@@ -1,5 +1,5 @@
 def is_leap_year(year):
-	if (year % 400 == 0) or (year % 100 ！= 0 and year % 4 == 0):
+	if (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0):
 		return True
 	else:
 		return False
